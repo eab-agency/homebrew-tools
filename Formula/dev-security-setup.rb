fdbdc5848019325b1acf47147b521eb6c58de76f
Andrettix <72987414+oleasky@users.noreply.github.com>
2026-02-06 17:20:05 -0500

    Update SHA256 after repo made public

--- Formula/dev-security-setup.rb
+++ Formula/dev-security-setup.rb
@@ -2,7 +2,7 @@ class DevSecuritySetup < Formula
   desc "One-command secret detection pipeline for Git projects"
   homepage "https://github.com/eab-agency/dev-security-setup"
   url "https://github.com/eab-agency/dev-security-setup/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "d2656a27e67143819bce43a3599928dc8b6651c25ec7526774cb64d5f96388f6"
   license :cannot_represent
 
   depends_on "pre-commit"
